5f5cfaad00328f3f587e48d2827e7c8f2118990e
Andrew Jackman <andrew@andrewjackman.com>
2020-04-24 15:01:11 +0100

    Added random number generator and first iteration of game.

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,14 @@
 module Main exposing (..)
 
 import Browser
+import Html exposing (div, h1, text)
+import Random
+
 
 {-| Either we haven't guessed yet, or we have a valid guess. Since users can
 enter any string into the input field and not just numbers,
-guesses may also be invalid. -}
+guesses may also be invalid.
+-}
 type Guess
     = NotGuessed
     | Guessed Int
@@ -13,14 +17,19 @@ type Guess
 
 {-| At first the game is in an initial state and needs to
 generate a random number. Once that's done the game can be played and we need
-to keep track of the secret number and a guess by the player. -}
+to keep track of the secret number and a guess by the player.
+-}
 type GameState
-      = Initializing
-      | Playing Int Guess
+    = Initializing
+    | Playing Int Guess
 
-type alias Model = { guessInput: String, state : GameState }
 
-type Msg = DoStuff
+type alias Model =
+    { guessInput : String, state : GameState }
+
+
+type Msg
+    = GeneratedNumber Int
 
 
 main : Program () Model Msg
@@ -32,8 +41,47 @@ main =
         , subscriptions = \_ -> Sub.none
         }
 
-init = Debug.todo "Implement me"
 
-update = Debug.todo "Implement me"
+{-| the initial model and number generating side-effect
+-}
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { guessInput = "", state = Initializing }, generateSecretNumber )
 
-view = Debug.todo "Implement me"
+
+{-| we're handling the message and are returning a new model-command tuple
+-}
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GeneratedNumber num ->
+            ( { model | state = Playing num NotGuessed }, Cmd.none )
+
+
+{-| We can set the document title and the body in the view.
+-}
+view : Model -> Browser.Document Msg
+view model =
+    { title = "PET Stack Guessing Game"
+    , body =
+        [ div []
+            [ h1 [] [ text "PET Stack Guessing Game" ]
+            , div []
+                [ case model.state of
+                    Initializing ->
+                        text "Generating Number"
+
+                    Playing secret guess ->
+                        text ("Our secret number is " ++ String.fromInt secret)
+                ]
+            ]
+        ]
+    }
+
+
+{-| We want integers between 0 and 100. Also we want the `GeneratedNumber`
+message to be triggered when the number has been generated
+-}
+generateSecretNumber : Cmd Msg
+generateSecretNumber =
+    Random.generate GeneratedNumber (Random.int 0 100)
