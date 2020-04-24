793705c3a304d9a36ba5deb72c5912d81cc110fe
Andrew Jackman <andrew@andrewjackman.com>
2020-04-24 14:50:38 +0100

    Add in the Main.elm.

---
+++ src/Main.elm
@@ -0,0 +1,23 @@
+module Main exposing (..)
+
+import Browser
+
+type alias Model = {}
+
+type Msg = DoStuff
+
+
+main : Program Int Model Msg
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+init = Debug.todo "Implement me"
+
+update = Debug.todo "Implement me"
+
+view = Debug.todo "Implement me"
